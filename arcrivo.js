13b1c03ffee8cb0418514e96fddf20389cb0137d
saddipaez-lgtm <saddipaez@gmail.com>
2026-06-30 10:52:00 -0400

    Refactor redirigir function to remove leading slash

--- arcrivo.js
+++ arcrivo.js
@@ -2,15 +2,15 @@ const boton =document.getElementById("b");
 function redirigir(){
     let nucleo =document.getElementById("T").value;
     if(nucleo=== "1"){
-        window.location.href = "/refineria.html"
+        window.location.href = "refineria.html"
     }
     else if(nucleo=== "2"){
-        window.location.href="/almacen.html";
+        window.location.href="almacen.html";
     }
     else if(nucleo=== "3"){
-        window.location.href="/bomba.html";
+        window.location.href="bomba.html";
     }
     else if(nucleo === "4"){
-        window.location.href="/admin.html"
+        window.location.href="admin.html"
     }
 }
