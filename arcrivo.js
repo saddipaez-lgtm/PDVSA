0b53c3d60914a91bb6bae1bc94ec32462ad82097
saddipaez-lgtm <saddipaez@gmail.com>
2026-06-30 10:51:08 -0400

    Fix redirection logic in arcrivo.js

--- arcrivo.js
+++ arcrivo.js
@@ -1,16 +1,16 @@
 const boton =document.getElementById("b");
 function redirigir(){
     let nucleo =document.getElementById("T").value;
-    if(nucleo=== ){
+    if(nucleo=== "1"){
         window.location.href = "/refineria.html"
     }
-    else if(nucleo=== 2){
+    else if(nucleo=== "2"){
         window.location.href="/almacen.html";
     }
-    else if(nucleo=== 3){
+    else if(nucleo=== "3"){
         window.location.href="/bomba.html";
     }
-    else if(nucleo === 4){
+    else if(nucleo === "4"){
         window.location.href="/admin.html"
     }
 }
