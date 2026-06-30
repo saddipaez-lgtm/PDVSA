e79d5d971e5cd675c64f63457ebd52915cdcfa22
saddipaez-lgtm <saddipaez@gmail.com>
2026-06-30 10:29:21 -0400

    s

---
+++ arcrivo.js
@@ -0,0 +1,16 @@
+const boton =document.getElementById("b");
+function redirigir(){
+    let nucleo =document.getElementById("T").value;
+    if(nucleo=== ){
+        window.location.href = "/refineria.html"
+    }
+    else if(nucleo=== 2){
+        window.location.href="/almacen.html";
+    }
+    else if(nucleo=== 3){
+        window.location.href="/bomba.html";
+    }
+    else if(nucleo === 4){
+        window.location.href="/admin.html"
+    }
+}
